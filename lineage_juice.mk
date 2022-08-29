ebc8761b16b95c88ec108777084be0fc2fc71ee7
darknius20 <darknius21@gmail.com>
2022-08-29 19:02:15 +0000

    Ricedroid Bringup

--- lineage_juice.mk
+++ lineage_juice.mk
@@ -19,6 +19,33 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
 
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := Darknius
+
+# chipset flag (one word only/no spacing)
+RICE_CHIPSET := bengal
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := false
+
+# Sushi Bootanimation (only 1080p supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# Graphene Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := false
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
+
+# Quick tap feature
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 # Fingerprint
 BUILD_FINGERPRINT := POCO/citrus_eea/citrus:11/RKQ1.201004.002/V12.5.3.0.RJFEUXM:user/release-keys
 
