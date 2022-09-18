d08e77605dd3fd2ead6cb7d492527e0241563db9
Darknius Lighntess <darknius21@gmail.com>
2022-09-18 03:02:33 -0700

    Edit anu

--- evolution_juice.mk
+++ evolution_juice.mk
@@ -6,8 +6,13 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common evolution stuff.
+# Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := UNOFFICIAL
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := evolution_juice
@@ -15,19 +20,6 @@ PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
-
-
-# GApps
-WITH_GAPPS := false
-TARGET_SHIP_PREBUILT_APEX := true
-TARGET_GAPPS_ARCH := arm64
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Resolution of Bootanimation
-TARGET_BOOT_ANIMATION_RES := 1080
 
 # Fingerprint
 BUILD_FINGERPRINT := google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys
